4ec6b289cc82b11acf6f133937360053b8e1aa13
Ryan Costa <89921782+ryanfcosta@users.noreply.github.com>
2025-09-12 21:21:12 -0300

    init

---
+++ lostinbits.py
@@ -0,0 +1,45 @@
+import pygame
+from PPlay import window, sprite, keyboard
+
+janela = window.Window(1920, 1080)
+janela.set_title("Lost in Bits")
+fundo = sprite.Sprite("fundo.png")
+fundo.set_position(0, 0)
+
+chaoY = 870  
+personagem = sprite.Sprite("tst.png")
+personagem.set_position((janela.width - personagem.width)/2, chaoY - personagem.height)
+
+teclado = keyboard.Keyboard()
+velX = 300 
+velY = 0
+grav = 1200 
+forcaPulo = -400  
+onFloor = True
+
+while True:
+    deltaTime = janela.delta_time()
+    
+    if (teclado.key_pressed("a") or teclado.key_pressed("LEFT")) and personagem.x > 0:
+        personagem.x -= velX * deltaTime
+    if (teclado.key_pressed("d") or teclado.key_pressed("RIGHT")) and personagem.x < janela.width - personagem.width:
+        personagem.x += velX * deltaTime
+
+    if (teclado.key_pressed("w") or teclado.key_pressed("UP")) and onFloor:
+        velY = forcaPulo
+        onFloor = False
+    
+    if not onFloor:
+        velY += grav * deltaTime
+        personagem.y += velY * deltaTime
+        if personagem.y + personagem.height >= chaoY:
+            personagem.y = chaoY - personagem.height
+            velY = 0
+            onFloor = True
+    
+    if (teclado.key_pressed("s") or teclado.key_pressed("DOWN")) and onFloor:
+        pass
+    
+    fundo.draw()
+    personagem.draw()
+    janela.update()
