29de0745e082309dac0950178179c5663eed6554
Biel675 <biel675.contato@gmail.com>
2025-11-08 22:37:42 -0300

    Cleanup imports and unused code

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from PPlay import window, sprite
+from PPlay import window
 import pygame
-import game_manager, abstract_level, plumber_level
+import game_manager, plumber_level
 
 window = window.Window(1920, 1080)
 
 pygame.init()
 pygame.display.set_mode((window.width, window.height), pygame.FULLSCREEN)
-
-#player = sprite.Sprite(path + "josh_right.png")
 
 game = game_manager.Game(window)
 plumber_level = plumber_level.PlumberLevel(window, "level_1/", "background")
