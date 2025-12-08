e0e1f800465649ad61c68779565f6290b49c5e66
Biel675 <biel675.contato@gmail.com>
2025-12-08 16:57:52 -0300

    Add rewind ability (with debug)

--- entity.py
+++ entity.py
@@ -1,5 +1,6 @@
 from PPlay import sprite
-from constants import UP, LEFT, RIGHT, DOWN
+from constants import UP, LEFT, RIGHT, DOWN, STATES_PER_SECOND, REWIND_DURATION_SECS
+from collections import deque
 
 class Entity:    
     def __init__(self, window, level):
@@ -11,6 +12,10 @@ class Entity:
         self.direction_y = None
         self.x_left_limit = 0
         self.x_right_limit = 1
+        self.alive = True
+        
+        total_states = STATES_PER_SECOND * REWIND_DURATION_SECS
+        self.states = deque(maxlen=total_states)
     
     def setup_sprite(self, assets_path, sprite_image):
         self.sprite = sprite.Sprite(f"assets/{assets_path}{sprite_image}.png")
@@ -35,3 +40,35 @@ class Entity:
     def set_direction_x(self, direction_x):
         if direction_x is not None:
             self.direction_x = direction_x
+    
+    def load_state(self, state):
+        self.sprite.x = state.x
+        self.sprite.y = state.y
+        self.direction_x = state.direction_x
+        self.direction_y = state.direction_y
+        self.velocity_x = state.velocity_x
+        self.velocity_y = state.velocity_y
+        self.alive = state.alive
+    
+    def save_state(self):
+        state = _EntityState(
+            self.sprite.x,
+            self.sprite.y,
+            self.direction_x,
+            self.direction_y,
+            self.velocity_x,
+            self.velocity_y,
+            self.alive
+        )
+
+        self.states.append(state)
+
+class _EntityState:
+    def __init__(self, x, y, direction_x, direction_y, velocity_x, velocity_y, alive):
+        self.x = x
+        self.y = y
+        self.direction_x = direction_x
+        self.direction_y = direction_y
+        self.velocity_x = velocity_x
+        self.velocity_y = velocity_y
+        self.alive = alive
--- game_manager.py
+++ game_manager.py
@@ -1,10 +1,14 @@
 from PPlay import window, sprite
 import player
-from constants import UP, LEFT, RIGHT, DOWN, GRAVITY
+from constants import UP, LEFT, RIGHT, DOWN, GRAVITY, STATES_PER_SECOND, REWIND_DURATION_SECS
 import main_menu
 
+TIME_PER_SAVE = 1.0 / STATES_PER_SECOND
+TIME_PER_STATE = 1.0 / (STATES_PER_SECOND * 2)
+
 class Game:
-    menu = "main_menu"
+    #menu = "main_menu"
+    menu = "game"
     up_move_key = "W"
     left_move_key = "A"
     right_move_key = "D"
@@ -14,6 +18,13 @@ class Game:
         self.keyboard = window.get_keyboard()
         self.level = None
         self.main_menu = main_menu.MainMenu(window, "menu/", self)
+        
+        self.collected_cartridges = 0
+        
+        self.is_rewinding = False
+        self.current_rewind_states = 0
+        self.save_state_counter = 0.0
+        self.rewind_state_counter = 0.0
     
     def setup_level(self):
         self.level.load_level()
@@ -48,32 +59,93 @@ class Game:
     
     def level_loop(self):
         delta_time = self.window.delta_time()
-        
-        player_input_direction_x = self.get_player_input_direction_x(self.keyboard)
-        self.level.player.set_direction_x(player_input_direction_x)
-        
-        player_input_direction_y = self.get_player_input_direction_y(self.keyboard)
-        self.level.player.set_direction_y(player_input_direction_y)
 
-        self.level.player.move(delta_time)
-        
+        # Drawing logic
         self.level.background.draw()
         self.level.player.sprite.draw()
         
         for npc in self.level.npcs:
             if npc.alive:
-                npc.move(delta_time)
                 npc.sprite.x += self.level.background.x
                 npc.sprite.draw()
                 npc.sprite.x -= self.level.background.x
-        
         for platform in self.level.platforms:
             platform.draw()
         
         self.level.door.draw()
-        
         self.window.draw_text(self.level.level_name, self.window.width - 350, 20, size=30, color=(0, 0, 0), font_name="Arial", bold=True)
+
+        
+        if self.is_rewinding:
+            self.rewind_step(delta_time)
+            return
+
+        if self.keyboard.key_pressed("L"):
+            self.collected_cartridges += 1
+            print(self.collected_cartridges)
+        
+        if self.keyboard.key_pressed("X") and self.collected_cartridges >= 4:
+            self.start_rewind_ability()
+        
+        self.save_states(delta_time)
+        
+        player_input_direction_x = self.get_player_input_direction_x(self.keyboard)
+        self.level.player.set_direction_x(player_input_direction_x)
+        player_input_direction_y = self.get_player_input_direction_y(self.keyboard)
+        self.level.player.set_direction_y(player_input_direction_y)
+        
+        self.level.player.move(delta_time)
+        for npc in self.level.npcs:
+            if npc.alive:
+                npc.move(delta_time)
+        
         self.level.handle_player_collisions()
+    
+    def save_states(self, delta_time):
+        self.save_state_counter += delta_time
+
+        if self.save_state_counter >= TIME_PER_SAVE:
+            while self.save_state_counter >= TIME_PER_SAVE:
+                self.level.player.save_state()
+                for npc in self.level.npcs:
+                    npc.save_state()
+                
+                self.save_state_counter -= TIME_PER_SAVE
+    
+    def start_rewind_ability(self):
+        self.is_rewinding = True
+        self.current_rewind_states = len(self.level.player.states)
+        self.collected_cartridges -= 4
+    
+    def rewind_step(self, delta_time):
+        if self.current_rewind_states <= 0:
+            self.current_rewind_states = 0
+            self.rewind_state_counter = 0.0
+            self.is_rewinding = False
+            return
+        
+        self.rewind_state_counter += delta_time
+
+        if self.rewind_state_counter >= TIME_PER_STATE:
+            while self.rewind_state_counter >= TIME_PER_STATE:
+                if self.current_rewind_states <= 0:
+                    self.rewind_state_counter = 0.0
+                    break
+
+                if self.level.player.states:
+                    state = self.level.player.states.pop()
+                    self.level.player.load_state(state)
+                
+                for npc in self.level.npcs:
+                       if npc.states:
+                        state = npc.states.pop()
+                        npc.load_state(state)
+                
+                self.current_rewind_states -= 1
+                self.rewind_state_counter -= TIME_PER_STATE
+        
+        if self.current_rewind_states <= 0:
+            self.is_rewinding = False
     
     # yet to be done
     def has_game_saved(self):
